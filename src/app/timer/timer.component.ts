16b9b6b7f2b38010c0199c81d7f2b509df6864f2
Sam Farley <samanthafarley32@gmail.com>
2022-04-19 16:46:07 -0400

    added stop timer feature

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -9,6 +9,7 @@ export class TimerComponent implements OnInit {
   minutesLeft: number = 0;
   secondsLeft: number = 0;
   totalSecondsLeft: number = this.minutesLeft*60 + this.secondsLeft;
+  myInterval?: number;
 
   constructor() { }
 
@@ -20,10 +21,32 @@ export class TimerComponent implements OnInit {
     if (this.totalSecondsLeft === 0 || !this.secondsLeft && !this.minutesLeft) {
       alert("Please set the timer!")
     } else {
-      while (this.totalSecondsLeft > 0) {
-        this.totalSecondsLeft--;
-      }
+      this.myInterval = window.setInterval(() => {
+        this.secondsLeft--;
+        if (this.totalSecondsLeft % 60 === 0 && this.totalSecondsLeft !== 0) {
+          this.minutesLeft--;
+          this.secondsLeft = 59;
+        }
+        this.totalSecondsLeft = this.minutesLeft*60 + this.secondsLeft;
+        if (this.totalSecondsLeft <= 0) {
+          clearInterval(this.myInterval);
+          return;
+        }
+      }, 1000);
     }
+  }
+
+  stopTimer(): void {
+    if (this.myInterval) {
+      clearInterval(this.myInterval);
+      this.totalSecondsLeft = 0;
+      this.minutesLeft = 0;
+      this.secondsLeft = 0;
+    }
+  }
+
+  pauseTimer(): void {
+    
   }
 
 }
