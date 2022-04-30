ed1d388f55e6f9b6d56876915260aeb5e044bc33
Sam Farley <samanthafarley32@gmail.com>
2022-04-29 22:23:06 -0400

    timer styles, added audio

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION, ViewChild, ElementRef } from '@angular/core';
+// sound effects from https://www.tones7.com/ringtones/sound-effects/
 
 @Component({
   selector: 'app-timer',
@@ -10,6 +11,13 @@ export class TimerComponent implements OnInit {
   secondsLeft: number = 0;
   totalSecondsLeft: number = this.minutesLeft*60 + this.secondsLeft;
   myInterval?: number;
+  isPaused: boolean = false;
+
+  // variables for number input fields
+  @ViewChild("minTimerField") 
+  minTimerField?: ElementRef;
+  @ViewChild("secTimerField") 
+  secTimerField?: ElementRef;
 
   constructor() { }
 
@@ -17,20 +25,40 @@ export class TimerComponent implements OnInit {
   }
 
   startTimer(): void {
-    this.totalSecondsLeft = this.minutesLeft*60 + this.secondsLeft;
-    if (this.totalSecondsLeft === 0 || !this.secondsLeft && !this.minutesLeft) {
+    if (!this.minTimerField?.nativeElement.value && !this.secTimerField?.nativeElement.value) {
       alert("Please set the timer!")
     } else {
+      this.secondsLeft = this.secTimerField?.nativeElement.value;
+      this.minutesLeft = this.minTimerField?.nativeElement.value;
+
+      // if minutes field isn't filled out, set value to 0 (makes timer 00:30 instead of 0:30)
+      if (!this.minTimerField?.nativeElement.value) { this.minutesLeft = 0; } 
+      // if seconds field isn't filled out, set value to 0 (makes timer 10:00 instead of 10:0)
+      if (!this.secTimerField?.nativeElement.value) { this.secondsLeft = 0; } 
+
+      this.totalSecondsLeft = this.minutesLeft*60 + this.secondsLeft;
+
       this.myInterval = window.setInterval(() => {
-        this.secondsLeft--;
-        if (this.totalSecondsLeft % 60 === 0 && this.totalSecondsLeft !== 0) {
-          this.minutesLeft--;
-          this.secondsLeft = 59;
-        }
-        this.totalSecondsLeft = this.minutesLeft*60 + this.secondsLeft;
-        if (this.totalSecondsLeft <= 0) {
-          clearInterval(this.myInterval);
-          return;
+        if (!this.isPaused) {
+          this.secondsLeft--;
+          if (this.totalSecondsLeft % 60 === 0 && this.totalSecondsLeft !== 0) {
+            this.minutesLeft--;
+            this.secondsLeft = 59;
+          }
+          this.totalSecondsLeft = this.minutesLeft*60 + this.secondsLeft;
+          if (this.totalSecondsLeft <= 0) {
+            clearInterval(this.myInterval);
+            let audio = new Audio("");
+            audio.src = "../../assets/remix.mp3";
+            audio.load();
+            audio.currentTime = 1.75;
+            audio.play();
+            setTimeout(()=> {
+              audio.pause();
+              audio.currentTime = 1.75;
+              return;
+            }, 7000);
+          }
         }
       }, 1000);
     }
@@ -42,11 +70,12 @@ export class TimerComponent implements OnInit {
       this.totalSecondsLeft = 0;
       this.minutesLeft = 0;
       this.secondsLeft = 0;
+      this.isPaused = false;
     }
   }
 
   pauseTimer(): void {
-    
+    this.isPaused = !this.isPaused;
   }
 
 }
